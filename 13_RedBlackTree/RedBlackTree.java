8db7dba844adc19a1eb70e669448fd4b4e8b2ccd
2541989802 <2541989802@qq.com>
2022-07-15 16:44:16 -0400

    red black tree - start

---
+++ 13_RedBlackTree/RedBlackTree.java
@@ -0,0 +1,7 @@
+package redblacktree;
+
+import binarysearchtree.*;
+
+public class RedBlackTree<T extends Comparable<T>> extends BinarySearchTree<T>{
+    public class Node<T extends Comparable<T>> extends BinarySearchTree<T>.Node<T>{}
+}
