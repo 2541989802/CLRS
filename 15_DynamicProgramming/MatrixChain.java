8049e1d584cbd8289838b5df33b638c5bca11c69
dsong06 <dsong06@syr.edu>
2022-07-26 11:16:31 -0400

    dynamic programming, matrix chain

---
+++ 15_DynamicProgramming/MatrixChain.java
@@ -0,0 +1,25 @@
+package dynamicprogramming;
+
+public class MatrixChain{
+    public int[][] order(int[] mn){
+        int[][] res = new int[mn.length-1][mn.length-1];
+        int[][] cost = new int[mn.length-1][mn.length-1];
+        int t = 0;
+        for(int i=0; i<mn.length-1; i++){
+            for(int j=0; j<mn.length-1-i; j++){
+                if(i==0){
+                    cost[j][j+i]=0;
+                    res[j][j+i]=j+i;
+                }
+                for(int k=0; k<i; k++){
+                    t=mn[j]*mn[k+j+1]*mn[i+j+1]+cost[j][j+k]+cost[j+k][i+j];
+                    if(t<cost[j][j+i]){
+                        cost[j][j+i]=t;
+                        res[j][j+i]=k+1;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
